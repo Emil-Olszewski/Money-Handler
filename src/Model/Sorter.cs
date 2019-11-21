18ddae879b9a9177c20c82af7083e2e78dc606d8
Emil Olszewski <olszewski.emil13@gmail.com>
2019-11-21 05:06:01 +0100

    Add Sorter

---
+++ src/Model/Sorter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Money_App.Model
+{
+    public static class Sorter
+    {
+        public static void SortAlphabetically(List<string> list)
+        {
+            bool changed;
+            do
+            {
+                changed = false;
+                for (int i = 1; i < list.Count; i++)
+                {
+                    if (IsAlphabeticallyFirst(list[i].ToUpper(), list[i - 1].ToUpper()))
+                    {
+                        string temp = list[i];
+                        list[i] = list[i - 1];
+                        list[i - 1] = temp;
+                        changed = true;
+                    }
+                }
+
+            } while (changed == true);
+        }
+
+        public static bool IsAlphabeticallyFirst(string what, string than)
+        {
+            int lenght = what.Length > than.Length ? than.Length : what.Length;
+            for (int i = 0; i < lenght; i++)
+            {
+                if (what[i] > than[i])
+                    return false;
+                else if (what[i] < than[i])
+                    return true;
+                else
+                    continue;
+            }
+
+            return what.Length < than.Length;
+        }
+    }
+}
---
+++ tests/Model/SorterTests.cs
@@ -0,0 +1,43 @@
+﻿using Money_App.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Money_App.Model.Tests
+{
+    [TestClass()]
+    public class SorterTests
+    {
+        [TestMethod()]
+        public void SortAlphabeticallyTest()
+        {
+            var list = new List<string>()
+            {
+                "abc", "cab", "bac", "acb", "cba", "bca"
+            };
+
+            Sorter.SortAlphabetically(list);
+
+            Assert.IsTrue(list[0] == "abc");
+            Assert.IsTrue(list[1] == "acb");
+            Assert.IsTrue(list[2] == "bac");
+            Assert.IsTrue(list[3] == "bca");
+            Assert.IsTrue(list[4] == "cab");
+            Assert.IsTrue(list[5] == "cba");
+        }
+
+        [TestMethod()]
+        public void IsAlphabeticallyFirstTest()
+        {
+            string[] strings =
+            {
+                "ab", "ba", "abcde", "abced",
+                "04yui", "02rrr", "00kak", "00kaka"
+            };
+
+            Assert.IsTrue(Sorter.IsAlphabeticallyFirst(strings[0], strings[1]));
+            Assert.IsTrue(Sorter.IsAlphabeticallyFirst(strings[2], strings[3]));
+            Assert.IsTrue(Sorter.IsAlphabeticallyFirst(strings[5], strings[4]));
+            Assert.IsTrue(Sorter.IsAlphabeticallyFirst(strings[6], strings[7]));
+        }
+    }
+}
