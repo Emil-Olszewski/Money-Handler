e16f5c395ad9181162cc7f27aa721451a820ed7a
Emil Olszewski <olszewski.emil13@gmail.com>
2019-11-21 05:08:45 +0100

    Add load and sort functions

--- src/Model/Categories.cs
+++ src/Model/Categories.cs
@@ -6,7 +6,22 @@ namespace Money_App.Model
 {
     public class Categories : IEnumerable<string>
     {
-        private readonly List<string> list = new List<string>();
+        private readonly List<string> list 
+            = new List<string>();
+
+        public Categories()
+        {
+            ExtractCategoriesFromTransactions();
+            Sorter.SortAlphabetically(list);
+        }
+
+        private void ExtractCategoriesFromTransactions()
+        {
+            var transactionsList = SaveManager.LoadTransactions("save.dat") ?? (new List<Transaction>());
+            foreach (var transaction in transactionsList)
+                if (transaction.Category.Length > 0 && !list.Contains(transaction.Category))
+                    list.Add(transaction.Category);
+        }      
 
         public void Add(string category)
             => list.Add(category);
